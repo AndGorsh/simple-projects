00bf7b7350b4eba0b6e29eb97a95cd72c96bbe46
AndGorsh <64104463+AndGorsh@users.noreply.github.com>
2020-05-05 20:23:25 +0200

    умножение как функция, цикл повторов операции, пока пользователь не скажет "нет"

---
+++ multyply_table.py
@@ -0,0 +1,22 @@
+"""проэкт"Таблица умножения".
+
+Пользователь вводит число, которое будет перемножаться
+на числа от 1 до 10.
+
+"""
+
+
+def i_x_m():
+    for multiplier in range(1,11):
+        print(i, "X", multiplier, "=", multiplier * i)
+
+next_step = "да"
+
+while next_step == "да":
+    i = int(input("Введите число: "))
+
+    i_x_m()
+    print("Хотите еще раз?")
+    next_step = input()
+    if next_step != "да":
+        print("К О Н Е Ц")
