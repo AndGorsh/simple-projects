a00af0a12f0186555825dd08a4a325fe8fa24235
AndGorsh <64104463+AndGorsh@users.noreply.github.com>
2020-05-06 16:40:01 +0200

    init player

--- space impact.py
+++ space impact.py
@@ -23,6 +23,15 @@ for side in range(4):
 border_pen.hideturtle()
 
 # create a player turtle
+player = turtle.Turtle()
+player.color("blue")
+player.shape("triangle")
+player.penup()
+player.speed(0)
+player.setposition(0, -250)
+player.setheading(90)
+
+
 
 
 
