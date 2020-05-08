ddc5b3aaa9532fed1e4486f76c9a1d4cc42c067b
AndGorsh <64104463+AndGorsh@users.noreply.github.com>
2020-05-08 13:44:49 +0200

    players setup

--- ping pong.py
+++ ping pong.py
@@ -6,6 +6,36 @@ wn.bgcolor("#2F4F4F")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("#FF6347")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("#F4A460")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+
+
+
+
+
+
+# BALL
+
+
+
+
+
 
 while True:
     wn.update()
